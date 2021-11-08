c969453db320ac6535a51f5fbc5b20c5bb680061
emerson_dalwampo <emerson.dalwampo@fireclay.ph>
2021-11-08 23:50:02 +0800

    fIX DATE

--- api/death_record/update.php
+++ api/death_record/update.php
@@ -2,6 +2,27 @@
 
 require_once('../config.php');
 
+$uuid = $_POST['uuid'];
+$record_no = $_POST['record_no'];
+$lname = $_POST['lname'];
+$fname = $_POST['fname'];
+$mname = $_POST['mname'];
+$birthday = $_POST['birthday'];
+$death_date = $_POST['death_date'];
+$address = $_POST['address'];
+$gender = $_POST['gender'];
+$relative = $_POST['relative'];
+$intermentDate = $_POST['intermentDate'];
 
+try {
+    $sql = "UPDATE death_records lname='$lname', fname='$fname', mname='$mname', birthday='$birthday', death_date='$death_date', address='$address', gender='$gender', relative='$relative', internment_date='$intermentDate' WHERE uuid='$uuid'";
+    
+    $result = $db->query($sql);
+    
+    echo json_encode($result);
+}
+catch (exception $e) {
+    echo json_encode(false);
+}
 
 ?>
--- api/funeral_customer/update.php
+++ api/funeral_customer/update.php
@@ -2,6 +2,27 @@
 
 require_once('../config.php');
 
+$uuid = $_POST['uuid'];
+$lname = $_POST['lname'];
+$fname = $_POST['fname'];
+$mname = $_POST['mname'];
+$contactNo = $_POST['contactNo'];
+$dueDate = $_POST['dueDate'];
+$downPayment = $_POST['downPayment'];
+$address = $_POST['address'];
+$gender = $_POST['gender'];
+$totalAmount = $_POST['totalAmount'];
+$amountPaid = $_POST['amountPaid'];
 
+try {
+    $sql = "UPDATE funeral_customers SET lname='$lname', fname='$fname', mname='$mname', contact_no='$contactNo', due_date='$dueDate', down_payment='$downPayment', address='$address',gender='$gender', total_amount=$totalAmount, amount_paid=$amountPaid WHERE uuid='$uuid'";
+    
+    $result = $db->query($sql);
+}
+catch (exception $e) {
+    $result = false;
+}
+
+echo json_encode($result);
 
 ?>
