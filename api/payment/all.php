d72026650d8dbf8cd920f48b281f272e2f2df319
sncrr <emersondalwampo1120@gmail.com>
2021-11-10 14:05:43 +0800

    Update all.php

--- api/payment/all.php
+++ api/payment/all.php
@@ -3,7 +3,7 @@
 require_once('../config.php');
 
 try {
-    $sql = "SELECT * FROM payments";
+    $sql = "SELECT payments.*, funeral_customers.lname, funeral_customers.fname, funeral_customers.mname FROM payments INNER JOIN funeral_customers ON payments.customer=funeral_customers.uuid";
     
     $result = $db->query($sql);
     
