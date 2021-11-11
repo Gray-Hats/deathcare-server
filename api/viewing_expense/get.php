ec0be596306dc50ff7c531d5f23cfa89f79c31d6
sncrr <emersondalwampo1120@gmail.com>
2021-11-11 17:41:16 +0800

    Update get.php

--- api/viewing_expense/get.php
+++ api/viewing_expense/get.php
@@ -9,11 +9,12 @@ try {
     
     $result = $db->query($sql);
     
-    $emparray = array();
-    while($row = $result->fetch_assoc()) {
-        $emparray[] = $row;
-    }
-    echo json_encode($emparray);
+    echo json_encode($result);
+    // $emparray = array();
+    // while($row = $result->fetch_assoc()) {
+    //     $emparray[] = $row;
+    // }
+    // echo json_encode($emparray);
 }
 catch (exception $e) {
     echo json_encode([]);
