f08df7c5626394eca88a95644d234df20666dd00
sncrr <emersondalwampo1120@gmail.com>
2021-11-11 15:16:12 +0800

    Update add.php

--- api/internment_expense/add.php
+++ api/internment_expense/add.php
@@ -2,6 +2,11 @@
 
 require_once('../config.php');
 
+if(!isset($_POST['contractNo'])) {
+    echo json_encode(false);
+    return;
+}
+
 $contractNo = $_POST['contractNo'];
 $hearse = $_POST['hearse'];
 $foods = $_POST['foods'];
@@ -15,8 +20,8 @@ $totalAmount = $_POST['totalAmount'];
 try {
     $sql = "INSERT INTO internment_expenses VALUES('$contractNo', $hearse, $foods, $refreshment, $balloons, $barangay, $staff, $other, $totalAmount)";
     
-    //$result = $db->query($sql);
-    echo json_encode($sql);
+    $result = $db->query($sql);
+    echo json_encode($result);
 }
 catch (exception $e) {
     echo json_encode($e);
