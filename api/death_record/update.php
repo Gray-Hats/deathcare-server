2cc5fd47f965edfd69caed3ad1dcef06fc60e957
sncrr <emersondalwampo1120@gmail.com>
2021-11-16 17:06:38 +0800

    Update update.php

--- api/death_record/update.php
+++ api/death_record/update.php
@@ -19,7 +19,7 @@ $relative = $_POST['relative'];
 $internmentDate = $_POST['internmentDate'];
 
 try {
-    $sql = "UPDATE death_records lname='$lname', fname='$fname', mname='$mname', birthday='$birthday', death_date='$death_date', address='$address', gender='$gender', relative='$relative', internment_date='$internmentDate' WHERE uuid='$uuid'";
+    $sql = "UPDATE death_records SET lname='$lname', fname='$fname', mname='$mname', birthday='$birthday', death_date='$death_date', address='$address', gender='$gender', relative='$relative', internment_date='$internmentDate' WHERE uuid='$uuid'";
     
     $result = $db->query($sql);
     
